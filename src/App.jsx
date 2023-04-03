716aa75838eb2096f2ed9423ef95ec1d7593fdcb
Joey Harbert <joey@actualize.co>
2023-04-03 15:51:37 -0500

    adds reformat so @ mentions show display name instead of user id

--- src/App.jsx
+++ src/App.jsx
@@ -31,10 +31,23 @@ function App() {
     let formatted_m = {};
     for (const [key, value] of Object.entries(m)) {
       formatted_m[key] = value.map((message) => {
+        //make sure each message has a user_profile
         if (!message.user_profile) {
           const user = users.filter((user) => user.id === message.user)?.[0];
 
           message.user_profile = user?.profile;
+        }
+        //replace @mentions with display name
+        if (message.text.match(/<@[A-Za-z0-9]*>/)) {
+          message.text.match(/<@[A-Za-z0-9]*>/).forEach((user) => {
+            const userIdd = user.substr(2, 11);
+            const userObject = users.filter((user) => user.id === userIdd)?.[0];
+            console.log(userObject.profile);
+            const userName = userObject.profile.display_name
+              ? userObject.profile.display_name
+              : userObject.profile.real_name;
+            message.text = message.text.replace(user, `<@${userName}>`);
+          });
         }
         return message;
       });
